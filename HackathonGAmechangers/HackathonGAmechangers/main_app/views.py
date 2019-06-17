6d617f51190d44e7a8707772afa37be8868e9847
Nick Bueltel <nickbueltel@gmail.com>
2019-06-17 16:28:43 -0500

    replaced old files (manual merge)

---
+++ HackathonGAmechangers/HackathonGAmechangers/main_app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
+from django.views.generic import ListView
+import uuid
+import boto3
+from .models import Photo, Scene
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'txstreetart'
+# Create your views here.
+
+class SceneCreate(CreateView):
+    model = Scene
+    fields = '__all__'
+
+class SceneUpdate(UpdateView):
+    model = Scene
+    fields = ['location', 'description']
+
+class SceneDelete(DeleteView):
+    model = Scene
+    success_url = '/scenes/'
+
+    
+def home(request):
+    return render(request, 'home.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def scenes_index(request):
+    scenes = Scene.objects.all()
+    return render(request,'scenes/index.html', { 'scenes': scenes })
+
+def scenes_detail(request, scene_id):
+    scene = Scene.objects.get(id=scene_id)
+    return render(request,'scenes/detail.html', { 'scene': scene })
+
+#todo
+
+def add_photo(request):
+    return render(request, 'home.html' )
+def login(request):
+    return render(request, 'login.html')
+
+def myposts(request):
+    return render(request, 'myposts.html')
