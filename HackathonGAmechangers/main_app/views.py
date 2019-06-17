50091a805a866f60077d99befce2493a5411e5da
Andrew Miscoe <armiscoe@gmail.com>
2019-06-17 16:57:22 -0500

    photo upload beginning

--- HackathonGAmechangers/main_app/views.py
+++ HackathonGAmechangers/main_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 import uuid
 import boto3
-from .models import Scene
+from .models import Scene, Photo
 
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'txstreetart'
@@ -39,8 +39,25 @@ def scenes_detail(request, scene_id):
 
 #todo
 
-def add_photo(request):
-    return render(request, 'home.html' )
+def add_photo(request, scene_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            
+            photo = Photo(url=url, scene_id=scene_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', scene_id=scene_id)
+
 def login(request):
     return render(request, 'login.html')
 
